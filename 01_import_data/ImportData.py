139621bfd6d8b0a7e9242ff64459ca971fd032c1
David Richard Steinmetz <davidsteinmetz@gmail.com>
2016-09-02 15:02:30 -0400

    update ImportData

--- 01_import_data/ImportData.py
+++ 01_import_data/ImportData.py
@@ -1,17 +1,20 @@
 # Switch to directory of file you wish to import
 
 import pandas as pd
-import ParseJSON
+from ParseJSON import *
 
 cd './01_External/01_Yelp/'
-bus = ParseJSON.ParseJSON(fileName='yelp_academic_dataset_business.json')
-chk = ParseJSON.ParseJSON(fileName='yelp_academic_dataset_checkin.json')
-rev = ParseJSON.ParseJSON(fileName='yelp_academic_dataset_review.json')
-tip = ParseJSON.ParseJSON(fileName='yelp_academic_dataset_tip.json')
-usr = ParseJSON.ParseJSON(fileName='yelp_academic_dataset_user.json')
+bus = ParseJSON(fileName='yelp_academic_dataset_business.json')
+chk = ParseJSON(fileName='yelp_academic_dataset_checkin.json')
+rev = ParseJSON(fileName='yelp_academic_dataset_review.json')
+tip = ParseJSON(fileName='yelp_academic_dataset_tip.json')
+usr = ParseJSON(fileName='yelp_academic_dataset_user.json')
 cd '../../'
 
 # Check to see the import worked
 bus = pd.DataFrame(bus)
 bus.head()
 bus.describe()
+
+States = ProcessID(bus, 'state')
+States[0]
