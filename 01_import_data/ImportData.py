a4ff90c50de718e45ccec956873f2bde85885f58
David Richard Steinmetz <davidsteinmetz@gmail.com>
2016-09-03 17:57:06 -0400

    update ImportData

--- 01_import_data/ImportData.py
+++ 01_import_data/ImportData.py
@@ -1,15 +1,16 @@
-# Switch to directory of file you wish to import
-
 import pandas as pd
 from ParseJSON import *
 
-cd './01_External/01_Yelp/'
-bus = ParseJSON(fileName='yelp_academic_dataset_business.json')
-chk = ParseJSON(fileName='yelp_academic_dataset_checkin.json')
-rev = ParseJSON(fileName='yelp_academic_dataset_review.json')
-tip = ParseJSON(fileName='yelp_academic_dataset_tip.json')
-usr = ParseJSON(fileName='yelp_academic_dataset_user.json')
-cd '../../'
+
+# Switch to directory of file you wish to import
+dir_path = './01_External/01_Yelp/'
+
+# Import data
+bus = ParseJSON(fileName=dir_path+'yelp_academic_dataset_business.json')
+chk = ParseJSON(fileName=dir_path+'yelp_academic_dataset_checkin.json')
+rev = ParseJSON(fileName=dir_path+'yelp_academic_dataset_review.json')
+tip = ParseJSON(fileName=dir_path+'yelp_academic_dataset_tip.json')
+usr = ParseJSON(fileName=dir_path+'yelp_academic_dataset_user.json')
 
 # Check to see the import worked
 bus = pd.DataFrame(bus)
@@ -17,4 +18,4 @@ bus.head()
 bus.describe()
 
 States = ProcessID(bus, 'state')
-States[0]
+States[0:5]
