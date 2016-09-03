654c637e6fbcc3e1ce685d4641fc802bfbe2adf3
David Richard Steinmetz <davidsteinmetz@gmail.com>
2016-09-03 18:51:07 -0400

    import example: three layers deep

--- 01_import_data/ImportData.py
+++ 01_import_data/ImportData.py
@@ -12,19 +12,8 @@ rev = ParseJSON(fileName=dir_path+'yelp_academic_dataset_review.json')
 tip = ParseJSON(fileName=dir_path+'yelp_academic_dataset_tip.json')
 usr = ParseJSON(fileName=dir_path+'yelp_academic_dataset_user.json')
 
-# Check to see the import worked
-bus = pd.DataFrame(bus)
-bus.head()
-bus.describe()
-
-States = ProcessID(bus, 'state')
-States[0:5]
-
-Hours = ProcessID(bus, 'hours')
-
+# Extract three levels deep
 attr = ProcessID(bus, 'attributes')
 amb = ProcessID(attr, 'Ambience')
-caters = ProcessID(attr, 'Caters')
+casual = ProcessID(amb, 'casual')
 
-
-amb = ProcessIDDavid(attr, 'Ambience')
