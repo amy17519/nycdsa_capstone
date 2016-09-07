c63d3b010bbc31314e255050106cf98cf930e866
David Richard Steinmetz <davidsteinmetz@gmail.com>
2016-09-07 19:53:22 -0400

    lat/long extraction in GetYelpData.py

--- 06_yelp_api/GetYelpData.py
+++ 06_yelp_api/GetYelpData.py
@@ -6,7 +6,7 @@ from yelp.client import Client
 from yelp.oauth1_authenticator import Oauth1Authenticator
 
 # read API keys
-with io.open('config_secret.json') as cred:
+with io.open('config_secret.json') as cred:  # Auth in file not shared on github
     creds = json.load(cred)
     auth = Oauth1Authenticator(**creds)
     client = Client(auth)
@@ -22,14 +22,32 @@ params = {
 
 # search_by_bounding_box takes a southwest latitude/longitude and a northeast
 # latitude/longitude as the location boundary
+sw_latitude, sw_longitude = 37.900000, -122.500000
+ne_latitude, ne_longitude = 37.788022, -122.399797
 response = client.search_by_bounding_box(
-    37.900000,
-    -122.500000,
-    37.788022,
-    -122.399797,
+    sw_latitude,
+    sw_longitude,
+    ne_latitude,
+    ne_longitude,
     **params
 )
 
+
+# Function to extract response locations
+def get_response_coords(resp):
+    coord = []
+
+    for i in range(len(resp.businesses)):
+        latitude = resp.businesses[i].location.coordinate.latitude
+        longitude = resp.businesses[i].location.coordinate.longitude
+        if latitude and longitude:
+            coord.append((latitude, longitude))
+
+    return coord
+
+# Retrieve latitude and longitude from the response object
+get_response_coords(response)
+
 # Parse response documentation:
 # https://www.yelp.com/developers/documentation/v2/search_api
-response.businesses[0].name
+# response.businesses[0].name
