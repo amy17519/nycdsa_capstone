e45e8cd9fbfaae39b5f5b4d481cb6f7af175d86d
David Richard Steinmetz <davidsteinmetz@gmail.com>
2016-09-16 17:02:46 -0400

    upd supervisor function: structure

--- 11_supervisor/Supervisor.py
+++ 11_supervisor/Supervisor.py
@@ -14,18 +14,23 @@ import numpy as np
 
 
 def supervisor(user_id, bounding_box, search_params):
-    # Test structures
+    # --- Test structures
     test_biz_cluster = pd.Series(np.random.randint(1, 25, 20))
     test_model_recs = pd.DataFrame({'cluster': np.random.randint(1, 25, 10)})
 
-    biz = get_businesses_on_map(bounding_box, search_params)    # Get businesses on map
-    # biz['cluster'] = get_map_clusters(biz)                    # Get clusters for biz on map
-    biz['cluster'] = test_biz_cluster                           # Get clusters for biz on map
-    sorted_biz = cluster_rating_sort(biz)                       # Sort biz by cluster and rating
+    # --- Collaborative filtering recommendations
     # gl_model = load_model('graphlab_model.pkl')               # Load graphlab model
     # model_recs = gl_model.predict(user_id)                    # Get model recommendations
     model_recs = test_model_recs                                # Get model recommendations
     model_clusters = get_model_clusters(model_recs)             # Get clusters for model recs
+
+    # --- Classify
+    biz = get_businesses_on_map(bounding_box, search_params)    # Get businesses on map
+    # biz['cluster'] = get_map_clusters(biz)                    # Classify biz on map to clusters (KNN)
+    biz['cluster'] = test_biz_cluster                           # Classify biz on map to clusters (KNN)
+    sorted_biz = cluster_rating_sort(biz)                       # Sort biz by cluster and rating
+
+    # --- Generate recommendations in new locale
     map_recs = gen_map_recs(sorted_biz, model_clusters)         # Generate recs from biz on map
 
     return map_recs
