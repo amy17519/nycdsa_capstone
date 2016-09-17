ff7d6d6aafff635634b1498e611946a057fc952a
David Richard Steinmetz <davidsteinmetz@gmail.com>
2016-09-17 14:04:40 -0400

    upd GLModel: include types

--- 98_deployment/scripts/GLModel.py
+++ 98_deployment/scripts/GLModel.py
@@ -6,12 +6,23 @@ def gl_model(user_id, num_rec):
     recommender = FG.GraphLabRecommmender()
     # recommender object
 
-    recommender.LoadModel(train='full', type='ranking', modelName='ranking',dir="./scripts")
+    recommender.LoadRestaurantTypeNMap(typeFile='diningOptions.csv',
+                                       mapFile='restaurants_types.csv',
+                                       typeDir='./')
+
+    recommender.LoadModel(train='full', type='ranking', modelName='ranking')
     # the model loaded from ./ranking/
+
+    # recommendation = recommender.Recommend(modelType='ranking',
+    #                                        user_ids=user_id,
+    #                                        k=num_rec)
 
     recommendation = recommender.Recommend(modelType='ranking',
                                            user_ids=user_id,
-                                           k=num_rec)
+                                           k=40,
+                                           business_types=['Chinese', 'American (New)', 'Others'])  # the list
+
     # user_id is the numpy array of user_id, k is the number of items to recommend
 
     return recommendation
+
